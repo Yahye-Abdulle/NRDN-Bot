01a35c3cc08c9b2bd5f161ed0b2c19e56cb3a70b
Yahye Abdulle <35307367+Yahye-Abdulle@users.noreply.github.com>
2018-10-18 18:34:33 +0100

    Update NRDN bot.py

--- NRDN bot.py
+++ NRDN bot.py
@@ -6,7 +6,7 @@ import time
 import random
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
@@ -18,38 +18,27 @@ async def on_message(message):
     #Declaring number of messages, as it increments depending on the number of messages purged
     numberofMsg = 0
     randomCoin = random.randint(0,1)
-    flipCoin = headorTails[randomCoin]
+    flipCoin = headorTails[1]
     
     #
     
-    if message.content.lower().startswith('!help'):
+    if message.content.lower().startswith('.help'):
         userID = message.author.id
-        await client.send_message(message.channel, '```Requested by <@%s> \n!purge \t !say \n!flip ```' % (userID),)
-    if message.content.lower().startswith('!purge'):
-        if message.author.id == '300999039867158528':
-            async for msg in client.logs_from(message.channel):
-                await client.delete_message(msg)
-                numberofMsg += 1
-            await client.send_message(message.channel, 'Deleted %s messages' % (numberofMsg))
-            time.sleep(0.5)
-            async for lastmsg in client.logs_from(message.channel):
-                await client.delete_message(lastmsg)
-        else:
-            await client.send_message(message.channel, 'You do not have permission')
-    if message.content.lower().startswith('!flip'):
-        await client.send_message(message.channel, 'Heads or Tails')
-        if message.content.lower().startswith('heads'):
-            if flipCoin == 'heads':
-                await client.send_message(message.channel, 'You have won')
-            else:
-                await client.send_message(message.channel, 'Sorry,you lose')
-        if message.content.lower().startswith('tails'):
-            if flipCoin == 'tails':
-                await client.send_message(message.channel, 'You have won')
-            else:
-                await client.send_message(message.channel, 'Sorry,you lose')
+        await client.send_message(message.channel, '```Requested by <@%s> .role ```' % (userID))
+    if message.content.lower().startswith('.role'):
+        await client.send_message(message.channel, '```European \t Asian```')
+    if message.content.lower().startswith('.european'):
+        roleEuropean = discord.utils.get(message.server.roles, id='502368967189331989')
+        await client.add_roles(message.author, roleEuropean)
+    if message.content.lower().startswith('.asian'):
+        roleAsian = discord.utils.get(message.server.roles, id='502369003650416642')
+        await client.add_roles(message.author, roleAsian)
+            
+    
+    
+            
             
 
 
 
-client.run('TOKEN')
+client.run('NTAyMzY3NDk4OTM5NjYyMzM2.Dqm6DA.41jXtnvc6Xku43kVSedHFw5qVQs')
